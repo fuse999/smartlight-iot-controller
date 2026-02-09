989d02371994f99824dd2cb704037cdc398a03bc
Riley Pence <48462386+fuse999@users.noreply.github.com>
2026-02-08 19:51:28 -0800

    fix

--- firmware/SmartLightESP32C6/secrets.example.h
+++ firmware/SmartLightESP32C6/secrets.example.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #define WIFI_SSID "YOUR_WIFI_NAME"
 #define WIFI_PASS "YOUR_WIFI_PASSWORD"
 #define SERVER_BASE "http://192.168.1.50:8001"
--- firmware/SmartLightESP32C6/secrets.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-static const char* WIFI_SSID = "Pence-Network";
-static const char* WIFI_PASS = "Tucker06";
-static const char* SERVER_BASE = "http://192.168.1.111:8000";
-static const char* DEVICE_TOKEN = "theonebulbtorulethemall";
