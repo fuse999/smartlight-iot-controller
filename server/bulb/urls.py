18dfdc388473a188a1f8927a1ce59841d7e20e9f
Riley Pence <48462386+fuse999@users.noreply.github.com>
2026-02-14 20:16:03 -0800

    Update urls.py

--- server/bulb/urls.py
+++ server/bulb/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from . import api_views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("control/", views.control_page, name="bulb-control"),
@@ -10,5 +10,5 @@ urlpatterns = [
     path("schedules/", views.schedules_page, name="bulb_schedules"),
     path("api/light/desired/", api_views.desired_state, name="api_desired_state"),
     path("api/light/report/", api_views.report_state, name="api_report_state"),
-    path("home/", home_view, name="home"),
+    path("home/", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
